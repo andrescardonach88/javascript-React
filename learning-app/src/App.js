f89be1765f569edcdf8b3b2724889a79aaea8de2
Jesús Garcia <jggarcia@prevalentware.com>
2022-05-21 08:56:32 -0500

    inicial

---
+++ learning-app/src/App.js
@@ -0,0 +1,10 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  //CODIGO LÓGICO DE LA VISTA VA AQUÍ
+
+  return <div className='App'>hola mundo</div>;
+}
+
+export default App;
